38967fa361d8a7994b4339d3462827a0ab602fce
xiaofeibao-xjtu <1441675923@qq.com>
2023-02-18 15:09:19 +0800

    Add module float divider

--- src/test/scala/gentest/GenTest.scala
+++ src/test/scala/gentest/GenTest.scala
@@ -1,10 +1,10 @@
 package yunsuan.gentest
 
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
-import yunsuan.vector.{VectorFloatAdder, VectorIntAdder}
+import yunsuan.vector.{FloatAdderReductionF32WidenF16, VectorFloatAdder, VectorFloatDivider, VectorFloatFMA, VectorIntAdder}
 
 object GenTest extends App {
   val path = """./generated/VectorFloatAdder"""
   (new ChiselStage).execute(Array("--emission-options=disableMemRandomization,disableRegisterRandomization",
-    "--target-dir", path), Seq(ChiselGeneratorAnnotation(() => new VectorFloatAdder())))
+    "--target-dir", path), Seq(ChiselGeneratorAnnotation(() => new VectorFloatDivider())))
 }
