fd6a502ea16d8c6cf14f08a66dd6e06720c65d60
czw <1753182770@qq.com>
2023-03-08 16:08:33 +0800

    func(VIAlu): add VIAlu code

---
+++ src/main/scala/yunsuan/vector/VectorALU/Permutation.scala
@@ -0,0 +1,110 @@
+
+package yunsuan.vector.alu
+
+import chisel3._
+import chisel3.util._
+import scala.language.postfixOps
+import yunsuan.vector._
+import yunsuan.vector.alu.VAluOpcode._
+
+class Permutation extends Module {
+  val VLEN = 128
+  val xLen = 64
+  val LaneWidth = 64
+  val NLanes = VLEN/64
+  val vlenb = VLEN/8
+  val io = IO(new Bundle() {
+    val vs1           = Input (UInt(VLEN.W))
+    val vs2           = Input (UInt(VLEN.W))
+    val vmask         = Input (UInt(VLEN.W))
+    val old_vd        = Input (UInt(VLEN.W))
+    val srcType       = Input (Vec(2, UInt(4.W)))  
+    val vdType        = Input (UInt(4.W))  
+    val op_code       = Input (UInt(6.W))       
+    val info          = Input(new VIFuInfo)
+    val valid         = Input(Bool())
+    val vd            = Output(UInt(VLEN.W))
+  })
+
+
+val vcompress = io.op_code === VAluOpcode.vcompress
+
+val (vs1, vs2, vmask, old_vd) = (io.vs1, io.vs2, io.vmask, io.old_vd)
+
+val vm     = io.info.vm     
+val ma     = io.info.ma     
+val ta     = io.info.ta     
+val lmul   = io.info.vlmul   
+val vl     = io.info.vl     
+val uopIdx = io.info.uopIdx 
+val fire   = io.valid
+val vsew   = io.srcType(1)(1,0)
+val signed = io.srcType(1)(2).asBool
+val widen  = io.vdType(1,0) === (io.srcType(1)(1,0) + 1.U) 
+val vsew_bytes = 1.U << vsew;
+val vsew_bits = 8.U << vsew;
+val ele_cnt = vlenb.U/vsew_bytes
+val vlRemain = Mux((vl >= (ele_cnt * uopIdx)), (vl - (ele_cnt * uopIdx)), 0.U) 
+val vlRemainBytes = vlRemain << vsew
+val current_res_boundary = Mux((vlRemainBytes > vlenb.U), vlenb.U, vlRemainBytes)
+
+val previous_ones_sum = Wire(UInt(7.W))
+val res_idx = Wire(Vec(vlenb, UInt(7.W)))
+val res_valid = Wire(Vec(vlenb, Bool()))
+val current_ones_sum = Wire(Vec(vlenb+1, UInt(7.W)))
+val base = Cat(uopIdx, 0.U(4.W))
+
+val vs1_bits = vs1 >> (ele_cnt * uopIdx)
+val vs1_byte_strb = Wire(Vec(vlenb, UInt(1.W))) 
+val cmprs_vd = Wire(Vec(vlenb, UInt(8.W))) 
+val cmprs_vd_reg = RegEnable(Cat(cmprs_vd.reverse), 0.U, fire) 
+
+for (i <-0 until vlenb) { 
+  when (i.U < vlRemainBytes) {
+    vs1_byte_strb(i) := vs1_bits(i)
+    when (vsew === 1.U(3.W)) {
+      vs1_byte_strb(i) := vs1_bits(i/2)
+    } .elsewhen (vsew === 2.U(3.W)) { 
+      vs1_byte_strb(i) := vs1_bits(i/4)
+    } .elsewhen (vsew === 3.U(3.W)){ 
+      vs1_byte_strb(i) := vs1_bits(i/8)
+    }
+  } .otherwise {
+    vs1_byte_strb(i) := 0.U 
+  }
+}
+
+previous_ones_sum := vmask(6,0)
+
+for (i <-0 until (vlenb+1)) {
+  current_ones_sum(i) := 0.U
+}
+
+for (i <-0 until vlenb) {
+  cmprs_vd(i) := 0.U
+  res_idx(i) := 0.U
+  res_valid(i) := false.B
+}
+
+current_ones_sum(0) := previous_ones_sum
+for (i <-0 until vlenb) {
+  cmprs_vd(i) := Mux(ta, "hff".U, old_vd(i*8+7, i*8))
+  current_ones_sum(i+1) := current_ones_sum(i) + vs1_byte_strb(i)
+  res_idx(i) := current_ones_sum(i+1) -base - 1.U
+  res_valid(i) := current_ones_sum(i+1) >= base + 1.U
+  when (vs1_byte_strb(i).asBool && res_valid(i) && (res_idx(i) < current_res_boundary)) {
+    cmprs_vd(res_idx(i)) := vs2(i*8+7, i*8)
+  } 
+}
+
+
+io.vd := cmprs_vd_reg
+
+}
+
+object VerilogPer extends App {
+  println("Generating the VPU CrossLane hardware")
+  emitVerilog(new Permutation(), Array("--target-dir", "generated"))
+}
+
+
---
+++ src/main/scala/yunsuan/vector/VectorALU/VIAlu.scala
@@ -0,0 +1,66 @@
+
+package yunsuan.vector.alu
+
+import chisel3._
+import chisel3.util._
+import yunsuan.vector._
+
+class VIAlu extends Module {
+  val io = IO(new Bundle {
+    val in = Flipped(ValidIO(new VIFuInput))
+    val out = ValidIO(new VIFuOutput)
+  })
+
+  // Latency of ALU is two cycles
+  val validS1 = RegInit(false.B)
+  validS1 := io.in.valid
+  io.out.valid := RegNext(validS1)
+
+  val vIntFixpAlu = Module(new VIntFixpAlu)
+  vIntFixpAlu.io.in := io.in.bits
+
+  val vReduAlu = Module(new Reduction)
+  vReduAlu.io.op_code := io.in.bits.opcode 
+  vReduAlu.io.info := io.in.bits.info
+  vReduAlu.io.srcType := io.in.bits.srcType
+  vReduAlu.io.vdType := io.in.bits.vdType
+  vReduAlu.io.vs1 := io.in.bits.vs1
+  vReduAlu.io.vs2 := io.in.bits.vs2
+  vReduAlu.io.vmask := io.in.bits.mask
+  vReduAlu.io.old_vd := io.in.bits.old_vd
+  vReduAlu.io.valid := io.in.valid
+
+  val vMaskAlu = Module(new VMask)
+  vMaskAlu.io.op_code := io.in.bits.opcode 
+  vMaskAlu.io.info := io.in.bits.info
+  vMaskAlu.io.srcType := io.in.bits.srcType
+  vMaskAlu.io.vdType := io.in.bits.vdType
+  vMaskAlu.io.vs1 := io.in.bits.vs1
+  vMaskAlu.io.vs2 := io.in.bits.vs2
+  vMaskAlu.io.vmask := io.in.bits.mask
+  vMaskAlu.io.old_vd := io.in.bits.old_vd
+  vMaskAlu.io.valid := io.in.valid
+
+  val vPermAlu = Module(new Permutation)
+  vPermAlu.io.op_code := io.in.bits.opcode 
+  vPermAlu.io.info := io.in.bits.info
+  vPermAlu.io.srcType := io.in.bits.srcType
+  vPermAlu.io.vdType := io.in.bits.vdType
+  vPermAlu.io.vs1 := io.in.bits.vs1
+  vPermAlu.io.vs2 := io.in.bits.vs2
+  vPermAlu.io.vmask := io.in.bits.mask
+  vPermAlu.io.old_vd := io.in.bits.old_vd
+  vPermAlu.io.valid := io.in.valid
+
+  val opcodeS1 = RegNext(io.in.bits.opcode)
+  val vdFinal = Mux(opcodeS1 < 33.U, vIntFixpAlu.io.out.vd,
+                Mux(opcodeS1 < 39.U, vReduAlu.io.vd, 
+                Mux(opcodeS1 < 46.U, vMaskAlu.io.vd, vPermAlu.io.vd)))
+  io.out.bits.vd := RegNext(vdFinal)
+  io.out.bits.vxsat := false.B         //!!!! Todo
+}
+
+object VerilogAlu extends App {
+  println("Generating the VALU hardware")
+  emitVerilog(new VIAlu(), Array("--target-dir", "generated"))
+}
