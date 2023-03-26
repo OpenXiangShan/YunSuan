701c2c909166facd53042f538a445bf0fedf3cb1
xiaofeibao-xjtu <1441675923@qq.com>
2023-03-26 19:57:56 +0800

    fix bug: VFF_NUM

--- src/test/csrc/include/vpu_constant.h
+++ src/test/csrc/include/vpu_constant.h
@@ -207,7 +207,7 @@ extern "C"{
 #define VFA_MUST_FRS1_OPTYPES {VFMERGE,VFMOVE,VFGT,VFGE}
 #define VFA_NEED_FRS1_OPTYPES {VFADD,VFSUB,VFMIN,VFMAX,VFSGNJ,VFSGNJN,VFSGNJX,VFEQ,VFNE,VFLT,VFLE}
 // vff funcop type
-#define VFF_NUM 1
+#define VFF_NUM 2
 #define VFMUL   (binstoi("0000"))
 #define VFMACC  (binstoi("0001"))
 #define VFNMACC (binstoi("0001"))
