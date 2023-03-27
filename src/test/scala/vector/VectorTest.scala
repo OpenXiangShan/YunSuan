afbfa7d7d3f625a3faa0b10a10a0e1efd4502e8d
xiaofeibao-xjtu <1441675923@qq.com>
2023-03-27 18:37:26 +0800

    func(VFMA):add vfmul.vf vfnmacc.vv vfnmacc.vf test(VFMA): add vfmul.vf vfnmacc.vv vfnmacc.vf

--- src/test/scala/vector/VectorTest.scala
+++ src/test/scala/vector/VectorTest.scala
@@ -15,9 +15,9 @@ import firrtl.AnnotationSeq
 import firrtl.options.TargetDirAnnotation
 
 object GenTest extends App {
-  val path = """./generated/VectorFloatAdder"""
+  val path = """./generated/VectorFloatFMA"""
   (new ChiselStage).execute(Array("--emission-options=disableMemRandomization,disableRegisterRandomization",
-    "--target-dir", path), Seq(ChiselGeneratorAnnotation(() => new VectorFloatAdder())))
+    "--target-dir", path), Seq(ChiselGeneratorAnnotation(() => new VectorFloatFMA())))
 }
 
 trait HasTestAnnos {
