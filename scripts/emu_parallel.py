f4eb9d7e1819e78ea8f3d41d65783ec6221d47d0
xiaofeibao-xjtu <1441675923@qq.com>
2023-04-06 20:12:30 +0800

    script: add script emu_parallel.py

---
+++ scripts/emu_parallel.py
@@ -0,0 +1,14 @@
+#!/bin/python
+# encoding: utf-8
+import os
+import sys
+import random
+import time
+
+os.system('mkdir ../log')
+random.seed()
+
+for i in range(int(sys.argv[1])):
+    seed_int = random.randint(1,(1<<31)-1)
+    time.sleep(3)
+    os.system('../build/emu -s {seed} -O {ops} --dump-wave -b 0 -e 0 2>../log/std_err_{seed}.log 1>../log/std_out_{seed}.log &'.format(seed = seed_int, ops = sys.argv[2]))
