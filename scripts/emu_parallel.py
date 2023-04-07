3b4075bbed39483117218e4892cbca81b510f32f
xiaofeibao-xjtu <1441675923@qq.com>
2023-04-07 15:51:30 +0800

    scripts: add -p for mkdir, change sleep 0.01

--- scripts/emu_parallel.py
+++ scripts/emu_parallel.py
@@ -5,10 +5,10 @@ import sys
 import random
 import time
 
-os.system('mkdir ../log')
+os.system('mkdir -p ../log')
 random.seed()
 
 for i in range(int(sys.argv[1])):
     seed_int = random.randint(1,(1<<31)-1)
-    time.sleep(3)
+    time.sleep(0.01)
     os.system('../build/emu -s {seed} -O {ops} --dump-wave -b 0 -e 0 2>../log/std_err_{seed}.log 1>../log/std_out_{seed}.log &'.format(seed = seed_int, ops = sys.argv[2]))
