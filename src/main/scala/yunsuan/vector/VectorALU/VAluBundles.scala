4a2d3ff569cb22ff8503cae836f76ed3f55718b7
zzy <2846728884@qq.com>
2023-09-21 16:02:16 +0800

    vector: remove useless codes

--- src/main/scala/yunsuan/vector/VectorALU/VAluBundles.scala
+++ src/main/scala/yunsuan/vector/VectorALU/VAluBundles.scala
@@ -88,7 +88,6 @@ class VIFuOutput extends Bundle {
 }
 
 class SewOH extends Bundle {  // 0   1   2   3
-  // val oneHot = Vec(4, Bool()) // 8, 16, 32, 64
   val oneHot = UInt(4.W) // b0-b3: 8, 16, 32, 64
   def is8 = oneHot(0)
   def is16 = oneHot(1)
@@ -98,7 +97,6 @@ class SewOH extends Bundle {  // 0   1   2   3
 object SewOH {
   def apply(vsew: UInt): SewOH = {
     val sew = Wire(new SewOH)
-    // sew.oneHot := VecInit(Seq.tabulate(4)(i => vsew === i.U))
     sew.oneHot := VecInit(Seq.tabulate(4)(i => vsew === i.U)).asUInt
     sew
   }
