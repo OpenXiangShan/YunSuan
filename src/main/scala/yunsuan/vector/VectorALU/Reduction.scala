d8503fc042794d4629f823f55931d853bea4d593
lewislzh <liuzehao19@mails.ucas.ac.cn>
2024-03-22 10:12:20 +0800

    fix vipu timing by adding a delay

--- src/main/scala/yunsuan/vector/VectorALU/Reduction.scala
+++ src/main/scala/yunsuan/vector/VectorALU/Reduction.scala
@@ -63,7 +63,26 @@ class Reduction extends Module {
   val vwredsum_vs = opcode.isVredsum && srcTypeVs2(2).asBool && (vdType(1, 0) === (srcTypeVs2(1, 0) + 1.U))
   val vwredsumu_vs = opcode.isVredsum && !srcTypeVs2(2).asBool && (vdType(1, 0) === (srcTypeVs2(1, 0) + 1.U))
   val vred_uop = opcode.isReduction
+  // to ensure timing requirements delay onecyle
+  val fire_reg_s1 = RegNext(fire)
+  val old_vd_reg_s1 = RegEnable(old_vd,0.U,fire)
+  val ta_reg_s1 = RegEnable(ta, false.B, fire)
+  val vl_reg_s1 = RegEnable(vl, 0.U, fire)
 
+  val vd_vsew_reg_s1 = RegEnable(vd_vsew,0.U,fire)
+  val eewVs2_reg_s1 = RegEnable(eewVs2.oneHot(2,0),0.U,fire)
+  val signed_reg_s1 = RegEnable(signed,false.B,fire)
+
+  val vredand_vs_reg_s1 = RegEnable(vredand_vs,false.B,fire)
+  val vredor_vs_reg_s1 = RegEnable(vredor_vs,false.B,fire)
+  val vredxor_vs_reg_s1 = RegEnable(vredxor_vs,false.B,fire)
+
+
+  val vdType_reg_s1 = RegEnable(vdType,0.U,fire)
+  val isVredmax_reg_s1 = RegEnable(opcode.isVredmax,false.B,fire)
+  val isVredmin_reg_s1 = RegEnable(opcode.isVredmin,false.B,fire)
+
+  // stage 0
   val vs12 = Cat(vs1, vs2)
   val shift = Mux(widen, uopIdx << vsew_plus1, uopIdx << (vsew_plus1 + 1.U))
   val vmask_bits = vmask >> shift
@@ -99,7 +118,7 @@ class Reduction extends Module {
   val ele16 = Wire(UInt(16.W))
   val ele32 = Wire(UInt(32.W))
   val ele64 = Wire(UInt(64.W))
-
+  // stage 0
   ele8 := 0.U
   when(fire) {
     when(vredmax_vs) {
@@ -155,6 +174,15 @@ class Reduction extends Module {
   val vs12m_bits_sew32 = Cat(vs12m_bytes_sew32.reverse)
   val vs12m_bits_sew64 = Cat(vs12m_bytes_sew64.reverse)
 
+  // to ensure timing requirements delay onecyle
+  val vs12m_bits_reg_s1 = RegEnable(vs12m_bits,0.U,fire)
+  val widen_reduction_uop_reg_s1 = RegEnable(widen_reduction_uop,false.B,fire)
+  val alu_uop_reg_s1 = RegEnable(alu_uop,false.B,fire)
+  val widen_alu_uop_reg_s1 = RegEnable(widen_alu_uop,false.B,fire)
+
+
+
+  // stage 0
   for (i <- 0 until 2 * vlenb) {
     vs12m_bytes_sew8(i) := vs12_bytes(i)
     when(((!vm && !vmask_bits(i)) || (i.U >= vlRemainBytes)) && (reduction_uop || widen_reduction_uop)) {
@@ -210,30 +238,35 @@ class Reduction extends Module {
   val lo_vs = Wire(UInt(128.W))
 
   val vd_logical_alu = Wire(UInt(128.W))
-
+  // stage 1
   vd_logical_alu := 0.U
-  when(vredand_vs) {
-    vd_logical_alu := vs12m_bits(2 * VLEN - 1, VLEN) & vs12m_bits(VLEN - 1, 0)
-  }.elsewhen(vredor_vs) {
-    vd_logical_alu := vs12m_bits(2 * VLEN - 1, VLEN) | vs12m_bits(VLEN - 1, 0)
-  }.elsewhen(vredxor_vs) {
-    vd_logical_alu := vs12m_bits(2 * VLEN - 1, VLEN) ^ vs12m_bits(VLEN - 1, 0)
+  when(vredand_vs_reg_s1) {
+    vd_logical_alu := vs12m_bits_reg_s1(2 * VLEN - 1, VLEN) & vs12m_bits_reg_s1(VLEN - 1, 0)
+  }.elsewhen(vredor_vs_reg_s1) {
+    vd_logical_alu := vs12m_bits_reg_s1(2 * VLEN - 1, VLEN) | vs12m_bits_reg_s1(VLEN - 1, 0)
+  }.elsewhen(vredxor_vs_reg_s1) {
+    vd_logical_alu := vs12m_bits_reg_s1(2 * VLEN - 1, VLEN) ^ vs12m_bits_reg_s1(VLEN - 1, 0)
   }
 
-  val sub = opcode.isVredmax || opcode.isVredmin
+  val sub = isVredmax_reg_s1 || isVredmin_reg_s1
 
   // Widen
-  val vs_hi_widen = Mux1H(eewVs2.oneHot(2, 0), Seq(8, 16, 32).map(sew =>
-    Cat(UIntSplit(vs12m_bits(127, 64), sew).map(BitsExtend(_, 2 * sew, signed)).reverse)))
-  val vs_lo_widen = Mux1H(eewVs2.oneHot(2, 0), Seq(8, 16, 32).map(sew =>
-    Cat(UIntSplit(vs12m_bits(63, 0), sew).map(BitsExtend(_, 2 * sew, signed)).reverse)))
-  hi_vs := Mux(widen_reduction_uop, vs_hi_widen, vs12m_bits(255, 128))
-  lo_vs := Mux(widen_reduction_uop, vs_lo_widen, vs12m_bits(127, 0))
+  // stage 1
+  val vs_hi_widen = Mux1H(eewVs2_reg_s1, Seq(8, 16, 32).map(sew =>
+    Cat(UIntSplit(vs12m_bits_reg_s1(127, 64), sew).map(BitsExtend(_, 2 * sew, signed_reg_s1)).reverse)))
+  val vs_lo_widen = Mux1H(eewVs2_reg_s1, Seq(8, 16, 32).map(sew =>
+    Cat(UIntSplit(vs12m_bits_reg_s1(63, 0), sew).map(BitsExtend(_, 2 * sew, signed_reg_s1)).reverse)))
+  hi_vs := Mux(widen_reduction_uop_reg_s1, vs_hi_widen, vs12m_bits_reg_s1(255, 128))
+  lo_vs := Mux(widen_reduction_uop_reg_s1, vs_lo_widen, vs12m_bits_reg_s1(127, 0))
 
   val vs1_zero = Wire(UInt(64.W))
   val vs1_zero_logical = Wire(UInt(64.W))
   val vd_logical = Wire(Vec(4, UInt(64.W)))
+  // to ensure timing requirements delay onecyle
+  val vs1_zero_reg_s1 = RegEnable(vs1_zero, 0.U, fire)
+  val vs1_zero_logical_reg_s1 = RegEnable(vs1_zero_logical, 0.U, fire)
 
+  // stage 0
   vs1_zero := Mux1H(eewVd.oneHot, Seq(8, 16, 32).map(n => Cat(Fill(xLen - n, 0.U), vs1(n - 1, 0))) :+ vs1(63, 0))
 
   vs1_zero_logical := Mux1H(eewVd.oneHot, Seq(8, 16, 32).map(n => Cat(Fill(xLen - n, 0.U), vs1(n - 1, 0))) :+ vs1(63, 0))
@@ -244,13 +277,13 @@ class Reduction extends Module {
   for (i <- 0 until 4) {
     vd_logical(i) := 0.U
   }
-
+  // stage 1
   when(vredand_vs) {
-    vd_logical(0) := vs1_zero_logical & vs12m_bits(127, 64) & vs12m_bits(63, 0)
+    vd_logical(0) := vs1_zero_logical_reg_s1 & vs12m_bits_reg_s1(127, 64) & vs12m_bits_reg_s1(63, 0)
   }.elsewhen(vredor_vs) {
-    vd_logical(0) := vs1_zero_logical | vs12m_bits(127, 64) | vs12m_bits(63, 0)
+    vd_logical(0) := vs1_zero_logical_reg_s1 | vs12m_bits_reg_s1(127, 64) | vs12m_bits_reg_s1(63, 0)
   }.elsewhen(vredxor_vs) {
-    vd_logical(0) := vs1_zero_logical ^ vs12m_bits(127, 64) ^ vs12m_bits(63, 0)
+    vd_logical(0) := vs1_zero_logical_reg_s1 ^ vs12m_bits_reg_s1(127, 64) ^ vs12m_bits_reg_s1(63, 0)
   }
 
   when(vredand_vs) {
@@ -276,27 +309,27 @@ class Reduction extends Module {
   }.elsewhen(vredxor_vs) {
     vd_logical(3) := vd_logical(2)(15, 8) ^ vd_logical(2)(7, 0)
   }
-
+  // stage 1 to 2
   val vd_reg = RegInit(0.U(128.W))
-  val old_vd_reg = RegEnable(Mux(alu_uop || widen_alu_uop, lo_vs, old_vd), 0.U, fire)
-  val signed_reg = RegEnable(signed, false.B, fire)
-  val vd_vsew_reg = RegEnable(vd_vsew, 0.U, fire)
-  val ta_reg = RegEnable(ta, false.B, fire)
-  val vl_reg = RegEnable(vl, 0.U, fire)
-  val isVredmax_reg = RegEnable(opcode.isVredmax, false.B, fire)
-  val isVredmin_reg = RegEnable(opcode.isVredmin, false.B, fire)
-  val reg_vredand_vs = RegEnable(vredand_vs, false.B, fire)
-  val reg_vredor_vs = RegEnable(vredor_vs, false.B, fire)
-  val reg_vredxor_vs = RegEnable(vredxor_vs, false.B, fire)
-  val alu_uop_reg = RegEnable(alu_uop, false.B, fire)
-  val widen_alu_uop_reg = RegEnable(widen_alu_uop, false.B, fire)
-  val vdType_reg = RegEnable(vdType(1,0), 0.U, fire)
+  val old_vd_reg = RegEnable(Mux(alu_uop_reg_s1 || widen_alu_uop_reg_s1, lo_vs, old_vd_reg_s1), 0.U, fire_reg_s1)
+  val signed_reg = RegEnable(signed_reg_s1, false.B, fire_reg_s1)
+  val vd_vsew_reg = RegEnable(vd_vsew_reg_s1, 0.U, fire_reg_s1)
+  val ta_reg = RegEnable(ta_reg_s1, false.B, fire_reg_s1)
+  val vl_reg = RegEnable(vl_reg_s1, 0.U, fire_reg_s1)
+  val isVredmax_reg = RegEnable(isVredmax_reg_s1, false.B, fire_reg_s1)
+  val isVredmin_reg = RegEnable(isVredmin_reg_s1, false.B, fire_reg_s1)
+  val reg_vredand_vs = RegEnable(vredand_vs_reg_s1, false.B, fire_reg_s1)
+  val reg_vredor_vs = RegEnable(vredor_vs_reg_s1, false.B, fire_reg_s1)
+  val reg_vredxor_vs = RegEnable(vredxor_vs_reg_s1, false.B, fire_reg_s1)
+  val alu_uop_reg = RegEnable(alu_uop_reg_s1, false.B, fire_reg_s1)
+  val widen_alu_uop_reg = RegEnable(widen_alu_uop_reg_s1, false.B, fire_reg_s1)
+  val vdType_reg = RegEnable(vdType_reg_s1(1,0), 0.U, fire_reg_s1)
 
   val vd_vsew_bits_reg = 8.U << vd_vsew_reg
   val sub_reg = isVredmax_reg || isVredmin_reg
   val eewVd_reg = SewOH(vdType_reg)
   val reg_vred_logical = reg_vredand_vs || reg_vredor_vs || reg_vredxor_vs
-
+  // stage 2
   // alu
   val vs_hi = Wire(UInt(128.W))
   val vs_lo = Wire(UInt(128.W))
@@ -339,18 +372,19 @@ class Reduction extends Module {
   }
 
   alu_vd := Mux(reg_vred_logical, vd_reg, Mux(sub_reg, Cat(vd_max.reverse), Cat(vd.reverse)))
-
+  // end part stage 2
   // sew64 sum
   val sum_sew64 = Wire(UInt(64.W))
   val carry_sew64 = Wire(UInt(64.W))
   val vd_sew64 = Wire(UInt(64.W))
-
+  // stage 1
   val csa_3to2_sew64 = Module(new CSA3to2(width = 64))
-  csa_3to2_sew64.io.in_a := vs12m_bits_sew64(63, 0)
-  csa_3to2_sew64.io.in_b := vs12m_bits_sew64(127, 64)
-  csa_3to2_sew64.io.in_c := vs1_zero
+  csa_3to2_sew64.io.in_a := vs12m_bits_reg_s1(63, 0)
+  csa_3to2_sew64.io.in_b := vs12m_bits_reg_s1(127, 64)
+  csa_3to2_sew64.io.in_c := vs1_zero_reg_s1
   sum_sew64 := csa_3to2_sew64.io.out_sum
   carry_sew64 := csa_3to2_sew64.io.out_car
+  // stage 2
   vd_sew64 := vd_reg(127, 64) + vd_reg(63, 0)
 
   // sew32 sum
@@ -358,14 +392,14 @@ class Reduction extends Module {
   val carry_sew32 = Wire(Vec(2, UInt(32.W)))
   val sum_add_sew32 = Wire(UInt(32.W))
   val vd_sew32 = Wire(UInt(32.W))
-
+  // stage 1
   val csa_3to2_sew32_0 = Module(new CSA3to2(width = 32))
-  csa_3to2_sew32_0.io.in_a := vs12m_bits_sew32(31, 0)
-  csa_3to2_sew32_0.io.in_b := vs12m_bits_sew32(63, 32)
-  csa_3to2_sew32_0.io.in_c := vs12m_bits_sew32(95, 64)
+  csa_3to2_sew32_0.io.in_a := vs12m_bits_reg_s1(31, 0)
+  csa_3to2_sew32_0.io.in_b := vs12m_bits_reg_s1(63, 32)
+  csa_3to2_sew32_0.io.in_c := vs12m_bits_reg_s1(95, 64)
   sum_sew32(0) := csa_3to2_sew32_0.io.out_sum
   carry_sew32(0) := csa_3to2_sew32_0.io.out_car
-  sum_add_sew32 := vs12m_bits_sew32(127, 96) + vs1_zero(31, 0)
+  sum_add_sew32 := vs12m_bits_reg_s1(127, 96) + vs1_zero_reg_s1(31, 0)
 
   val csa_3to2_sew32_1 = Module(new CSA3to2(width = 32))
   csa_3to2_sew32_1.io.in_a := sum_sew32(0)
@@ -373,6 +407,7 @@ class Reduction extends Module {
   csa_3to2_sew32_1.io.in_c := sum_add_sew32
   sum_sew32(1) := csa_3to2_sew32_1.io.out_sum
   carry_sew32(1) := csa_3to2_sew32_1.io.out_car
+  // stage 2
   vd_sew32 := vd_reg(63, 32) + vd_reg(31, 0)
 
   // sew16 sum
@@ -384,10 +419,10 @@ class Reduction extends Module {
   val carry2_sew16 = Wire(UInt(16.W))
   val vd_sew16 = Wire(UInt(16.W))
 
-  val in0_sew16 = Cat(vs1_zero(15, 0), vs12m_bits_sew16(127, 0))
+  val in0_sew16 = Cat(vs1_zero_reg_s1(15, 0), vs12m_bits_reg_s1(127, 0))
   val in1_sew16 = Cat(Cat(sum0_sew16.reverse), Cat(carry0_sew16.reverse))
   val in2_sew16 = Cat(Cat(sum1_sew16.reverse), Cat(carry1_sew16.reverse))
-
+  // stage 1
   for (i <- 0 until 3) {
     val csa_3to2_sew16 = Module(new CSA3to2(width = 16))
     csa_3to2_sew16.io.in_a := in0_sew16(48 * i + 15, 48 * i + 0)
@@ -413,6 +448,7 @@ class Reduction extends Module {
   csa_4to2_sew16.io.in_d := in2_sew16(63, 48)
   sum2_sew16 := csa_4to2_sew16.io.out_sum
   carry2_sew16 := csa_4to2_sew16.io.out_car
+  // stage 2
   vd_sew16 := vd_reg(31, 16) + vd_reg(15, 0)
 
   // sew8 sum
@@ -426,11 +462,11 @@ class Reduction extends Module {
   val carry3_sew8 = Wire(UInt(8.W))
   val vd_sew8 = Wire(UInt(8.W))
 
-  val in0_sew8 = vs12m_bits_sew8(127, 0)
-  val in1_sew8 = Cat(vs1_zero(7, 0), Cat(sum0_sew8.reverse), Cat(carry0_sew8.reverse))
+  val in0_sew8 = vs12m_bits_reg_s1(127, 0)
+  val in1_sew8 = Cat(vs1_zero_reg_s1(7, 0), Cat(sum0_sew8.reverse), Cat(carry0_sew8.reverse))
   val in2_sew8 = Cat(Cat(sum1_sew8.reverse), Cat(carry1_sew8.reverse))
   val in3_sew8 = Cat(Cat(sum2_sew8.reverse), Cat(carry2_sew8.reverse))
-
+  // stage 1
   for (i <- 0 until 4) {
     val csa_4to2_sew8 = Module(new CSA4to2(width = 8))
     csa_4to2_sew8.io.in_a := in0_sew8(32 * i + 7, 32 * i + 0)
@@ -466,38 +502,41 @@ class Reduction extends Module {
   csa_4to2_sew8.io.in_d := in3_sew8(31, 24)
   sum3_sew8 := csa_4to2_sew8.io.out_sum
   carry3_sew8 := csa_4to2_sew8.io.out_car
+  // stage 2
   vd_sew8 := vd_reg(15, 8) + vd_reg(7, 0)
 
   // sew64 max/min
   val vd_max_sew64 = Wire(UInt(64.W))
-
+  // stage 1
   val compare_3to1_sew64 = Module(new compare_3to1(w = 64))
-  compare_3to1_sew64.io.a := vs12m_bits_sew64(63, 0)
-  compare_3to1_sew64.io.b := vs12m_bits_sew64(127, 64)
-  compare_3to1_sew64.io.c := vs1_zero
-  compare_3to1_sew64.io.max := opcode.isVredmax
-  compare_3to1_sew64.io.signed := signed
+  compare_3to1_sew64.io.a := vs12m_bits_reg_s1(63, 0)
+  compare_3to1_sew64.io.b := vs12m_bits_reg_s1(127, 64)
+  compare_3to1_sew64.io.c := vs1_zero_reg_s1
+  compare_3to1_sew64.io.max := isVredmax_reg_s1
+  compare_3to1_sew64.io.signed := signed_reg_s1
   vd_max_sew64 := compare_3to1_sew64.io.d
 
   // sew32 max/min
+  // stage 1
   val vd0_max_sew32 = Wire(Vec(2, UInt(32.W)))
   val vd1_max_sew32 = Wire(UInt(32.W))
 
   val compare_3to1_sew32 = Module(new compare_3to1(w = 32))
-  compare_3to1_sew32.io.a := vs12m_bits_sew32(31, 0)
-  compare_3to1_sew32.io.b := vs12m_bits_sew32(63, 32)
-  compare_3to1_sew32.io.c := vs12m_bits_sew32(95, 64)
-  compare_3to1_sew32.io.max := opcode.isVredmax
-  compare_3to1_sew32.io.signed := signed
+  compare_3to1_sew32.io.a := vs12m_bits_reg_s1(31, 0)
+  compare_3to1_sew32.io.b := vs12m_bits_reg_s1(63, 32)
+  compare_3to1_sew32.io.c := vs12m_bits_reg_s1(95, 64)
+  compare_3to1_sew32.io.max := isVredmax_reg_s1
+  compare_3to1_sew32.io.signed := signed_reg_s1
   vd0_max_sew32(0) := compare_3to1_sew32.io.d
 
   val compare0_2to1_sew32 = Module(new compare_2to1(w = 32))
-  compare0_2to1_sew32.io.a := vs12m_bits_sew32(127, 96)
-  compare0_2to1_sew32.io.b := vs1_zero(31, 0)
-  compare0_2to1_sew32.io.max := opcode.isVredmax
-  compare0_2to1_sew32.io.signed := signed
+  compare0_2to1_sew32.io.a := vs12m_bits_reg_s1(127, 96)
+  compare0_2to1_sew32.io.b := vs1_zero_reg_s1(31, 0)
+  compare0_2to1_sew32.io.max := isVredmax_reg_s1
+  compare0_2to1_sew32.io.signed := signed_reg_s1
   vd0_max_sew32(1) := compare0_2to1_sew32.io.c
 
+  //stage 2
   val compare1_2to1_sew32 = Module(new compare_2to1(w = 32))
   compare1_2to1_sew32.io.a := vd_reg(31, 0)
   compare1_2to1_sew32.io.b := vd_reg(63, 32)
@@ -506,20 +545,21 @@ class Reduction extends Module {
   vd1_max_sew32 := compare1_2to1_sew32.io.c
 
   // sew16 max/min
+  // stage 1
   val vd0_max_sew16 = Wire(Vec(3, UInt(16.W)))
   val vd1_max_sew16 = Wire(UInt(16.W))
-  val in0_max_sew16 = Cat(vs1_zero(15, 0), vs12m_bits_sew16(127, 0))
+  val in0_max_sew16 = Cat(vs1_zero_reg_s1(15, 0), vs12m_bits_reg_s1(127, 0))
 
   for (i <- 0 until 3) {
     val compare_3to1_sew16 = Module(new compare_3to1(w = 16))
     compare_3to1_sew16.io.a := in0_max_sew16(48 * i + 15, 48 * i + 0)
     compare_3to1_sew16.io.b := in0_max_sew16(48 * i + 31, 48 * i + 16)
     compare_3to1_sew16.io.c := in0_max_sew16(48 * i + 47, 48 * i + 32)
-    compare_3to1_sew16.io.max := opcode.isVredmax
-    compare_3to1_sew16.io.signed := signed
+    compare_3to1_sew16.io.max := isVredmax_reg_s1
+    compare_3to1_sew16.io.signed := signed_reg_s1
     vd0_max_sew16(i) := compare_3to1_sew16.io.d
   }
-
+  // stage 2
   val compare1_3to1_sew16 = Module(new compare_3to1(w = 16))
   compare1_3to1_sew16.io.a := vd_reg(15, 0)
   compare1_3to1_sew16.io.b := vd_reg(31, 16)
@@ -529,6 +569,7 @@ class Reduction extends Module {
   vd1_max_sew16 := compare1_3to1_sew16.io.d
 
   // sew8 max/min
+  // stage 1
   val vd0_max_sew8 = Wire(Vec(6, UInt(8.W)))
   val vd1_max_sew8 = Wire(Vec(2, UInt(8.W)))
   val vd2_max_sew8 = Wire(UInt(8.W))
@@ -536,21 +577,21 @@ class Reduction extends Module {
 
   for (i <- 0 until 5) {
     val compare_3to1_sew8 = Module(new compare_3to1(w = 8))
-    compare_3to1_sew8.io.a := vs12m_bits_sew8(24 * i + 7, 24 * i + 0)
-    compare_3to1_sew8.io.b := vs12m_bits_sew8(24 * i + 15, 24 * i + 8)
-    compare_3to1_sew8.io.c := vs12m_bits_sew8(24 * i + 23, 24 * i + 16)
-    compare_3to1_sew8.io.max := opcode.isVredmax
-    compare_3to1_sew8.io.signed := signed
+    compare_3to1_sew8.io.a := vs12m_bits_reg_s1(24 * i + 7, 24 * i + 0)
+    compare_3to1_sew8.io.b := vs12m_bits_reg_s1(24 * i + 15, 24 * i + 8)
+    compare_3to1_sew8.io.c := vs12m_bits_reg_s1(24 * i + 23, 24 * i + 16)
+    compare_3to1_sew8.io.max := isVredmax_reg_s1
+    compare_3to1_sew8.io.signed := signed_reg_s1
     vd0_max_sew8(i) := compare_3to1_sew8.io.d
   }
 
   val compare0_2to1_sew8 = Module(new compare_2to1(w = 8))
-  compare0_2to1_sew8.io.a := vs12m_bits_sew8(127, 120)
-  compare0_2to1_sew8.io.b := vs1_zero(7, 0)
-  compare0_2to1_sew8.io.max := opcode.isVredmax
-  compare0_2to1_sew8.io.signed := signed
+  compare0_2to1_sew8.io.a := vs12m_bits_reg_s1(127, 120)
+  compare0_2to1_sew8.io.b := vs1_zero_reg_s1(7, 0)
+  compare0_2to1_sew8.io.max := isVredmax_reg_s1
+  compare0_2to1_sew8.io.signed := signed_reg_s1
   vd0_max_sew8(5) := compare0_2to1_sew8.io.c
-
+  // stage 2
   for (i <- 0 until 2) {
     val compare_3to1_sew8 = Module(new compare_3to1(w = 8))
     compare_3to1_sew8.io.a := vd_reg(24 * i + 7, 24 * i + 0)
@@ -570,9 +611,10 @@ class Reduction extends Module {
 
   val sum_vd = Wire(UInt(64.W))
   val max_vd = Wire(UInt(64.W))
-  val logical_vd = Mux(alu_uop, vd_logical_alu, Cat(0.U((VLEN - 64).W), vd_logical(~vdType(1, 0))))
+  // stage 1
+  val logical_vd = Mux(alu_uop_reg_s1, vd_logical_alu, Cat(0.U((VLEN - 64).W), vd_logical(~vdType_reg_s1(1, 0))))
   val red_vd = Wire(UInt(VLEN.W))
-
+  // stage 2
   sum_vd := vd_sew64
   when(vd_vsew_reg === 0.U) {
     sum_vd := vd_sew8
@@ -602,35 +644,35 @@ class Reduction extends Module {
   val red_vd_tail_vd = (old_vd_reg & (vd_mask << vd_vsew_bits_reg)) | (red_vd & (vd_mask >> (VLEN.U - vd_vsew_bits_reg)))
 
   val red_vd_tail = Mux(vl_reg === 0.U, old_vd_reg, Mux(ta_reg, red_vd_tail_one, red_vd_tail_vd))
-
-  when(fire) {
-    when(vredand_vs || vredor_vs || vredxor_vs) {
+  // stage 1 to 2
+  when(fire_reg_s1) {
+    when(vredand_vs_reg_s1 || vredor_vs_reg_s1 || vredxor_vs_reg_s1) {
       vd_reg := logical_vd
-    }.elsewhen(opcode.isVredmax || opcode.isVredmin) {
-      when(alu_uop) {
+    }.elsewhen(isVredmax_reg_s1 || isVredmin_reg_s1) {
+      when(alu_uop_reg_s1) {
         vd_reg := hi_vs
       }.otherwise {
-        when(vd_vsew === 0.U) {
+        when(vd_vsew_reg_s1 === 0.U) {
           vd_reg := Cat(vd0_max_sew8.reverse)
-        }.elsewhen(vd_vsew === 1.U) {
+        }.elsewhen(vd_vsew_reg_s1 === 1.U) {
           vd_reg := Cat(vd0_max_sew16.reverse)
-        }.elsewhen(vd_vsew === 2.U) {
+        }.elsewhen(vd_vsew_reg_s1 === 2.U) {
           vd_reg := Cat(vd0_max_sew32.reverse)
-        }.elsewhen(vd_vsew === 3.U) {
+        }.elsewhen(vd_vsew_reg_s1 === 3.U) {
           vd_reg := vd_max_sew64
         }
       }
     }.otherwise {
-      when(alu_uop || widen_alu_uop) {
+      when(alu_uop_reg_s1 || widen_alu_uop_reg_s1) {
         vd_reg := hi_vs
       }.otherwise {
-        when(vd_vsew === 0.U) {
+        when(vd_vsew_reg_s1 === 0.U) {
           vd_reg := Cat(sum3_sew8, carry3_sew8)
-        }.elsewhen(vd_vsew === 1.U) {
+        }.elsewhen(vd_vsew_reg_s1 === 1.U) {
           vd_reg := Cat(sum2_sew16, carry2_sew16)
-        }.elsewhen(vd_vsew === 2.U) {
+        }.elsewhen(vd_vsew_reg_s1 === 2.U) {
           vd_reg := Cat(sum_sew32(1), carry_sew32(1))
-        }.elsewhen(vd_vsew === 3.U) {
+        }.elsewhen(vd_vsew_reg_s1 === 3.U) {
           vd_reg := Cat(sum_sew64, carry_sew64)
         }
       }
