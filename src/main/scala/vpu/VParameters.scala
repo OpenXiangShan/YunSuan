cae29c3c354eec2e63b4ebdebebbbc59cb5eb281
xu zhi <xuzhi@node041.bosccluster.com>
2025-02-24 10:41:19 +0800

    add vfa // rebase completed

--- src/main/scala/vpu/VParameters.scala
+++ src/main/scala/vpu/VParameters.scala
@@ -17,6 +17,38 @@ object VParams {
   val nVRFWritePorts = 2
 }
 
+object Vfaddfunc6 {
+  def width = 6
+  def dummy    = "b111111".U(width.W)
+  def fadd     = "b000000".U(width.W)
+  def fsub     = "b000010".U(width.W)
+  def fmin     = "b000100".U(width.W)
+  def fmax     = "b000110".U(width.W)
+  def fmerge   = "b010111".U(width.W)
+  def fmove    = "b010111".U(width.W)
+  def fsgnj    = "b001000".U(width.W)
+  def fsgnjn   = "b001001".U(width.W)
+  def fsgnjx   = "b001010".U(width.W)
+  def feq      = "b011000".U(width.W)
+  def fne      = "b011100".U(width.W)
+  def flt      = "b011011".U(width.W)
+  def fle      = "b011001".U(width.W)
+  def fgt      = "b011101".U(width.W)
+  def fge      = "b011111".U(width.W)
+  def fclass   = "b010011".U(width.W)
+  def fmv_f_s  = "b010000".U(width.W)
+  def fmv_s_f  = "b010000".U(width.W)
+  def fredsum_u = "b000001".U(width.W) // unordered
+  def fredmin   = "b000101".U(width.W)
+  def fredmax   = "b000111".U(width.W)
+  def fredsum_o = "b000011".U(width.W) // ordered
+
+  def fminm     = "b011110".U(width.W) // zfa extension
+  def fmaxm     = "b010011".U(width.W) // zfa extension
+  def fleq      = "b011100".U(width.W) // zfa extension
+  def fltq      = "b011011".U(width.W) // zfa extension
+}
+
 object VfaddOpCode {
   def dummy    = "b11111".U(5.W)
   def fadd     = "b00000".U(5.W)
@@ -47,7 +79,18 @@ object VfaddOpCode {
   def fltq     = "b11011".U(5.W)
 }
 
+
 object VectorElementFormat {
+  def width = 2
+  def b = "b00".U(width.W)
+  def h = "b01".U(width.W)  // f16
+  def w = "b10".U(width.W)  // f32
+  def d = "b11".U(width.W)  // f64
+
+  def apply() = UInt(width.W)
+}
+
+object Vfauop_format {
   def width = 2
   def b = "b00".U(width.W)
   def h = "b01".U(width.W)  // f16
--- src/main/scala/vpu/execution/VExecution.scala
+++ src/main/scala/vpu/execution/VExecution.scala
@@ -13,58 +13,46 @@ class VExecution extends Module {
   })
 
   val in = io.in
+  // output types
+  val vfa_out = Wire(new VExuOutput)
+  val vdiv_out = Wire(new VExuOutput)
+  
   // vector float adder
-  val vfa_vsrc = Vec(3, Vec(VLEN/XLEN, UInt(XLEN.W)))
-  for(j <- 0 until VLEN/XLEN){
-    vfa_vsrc(0)(j) := io.in.vSrc(0)(XLEN*(j+1)-1, XLEN*j)
-  }
-  for(j <- 0 until VLEN/XLEN){
-    vfa_vsrc(1)(j) := io.in.vSrc(1)(XLEN*(j+1)-1, XLEN*j)
-  }
-  for(j <- 0 until VLEN/XLEN){
-    vfa_vsrc(2)(j) := io.in.vSrc(3)(XLEN*(j+1)-1, XLEN*j)
-  }
+  val vfa = Module(new VectorEXUFloatAdder)
+  val vfa_uop = Wire(new Vfa_setuop)
+  vfa_uop.funct := Cat(in.vuop.ctrl.funct6, in.vuop.ctrl.funct3)
+  vfa_uop.vm := in.vuop.ctrl.vm
+  vfa_uop.vs1 := in.vuop.ctrl.lsrc(0)
+  vfa_uop.vs2 := in.vuop.ctrl.lsrc(1)
 
-  val sew = "b01".U
-  for (i <- 0 until (VLEN / XLEN)) {
-    val (src1, src2, src3) = (vfa_vsrc(0)(i), vfa_vsrc(1)(i), vfa_vsrc(2)(i))
-    val vfa = Module(new VectorFloatAdder) // result at next cycle}
-    require(vfa.io.fp_a.getWidth == XLEN)
-    vfa.io.fire := busy
-    vfa.io.fp_a := src1
-    vfa.io.fp_b := src2
-    vfa.io.widen_a := Cat(vfa_vsrc(0)(1)(31+i*32,0+i*32),vfa_vsrc(0)(0)(31+i*32,0+i*32))
-    vfa.io.widen_b := Cat(vfa_vsrc(1)(1)(31+i*32,0+i*32),vfa_vsrc(1)(0)(31+i*32,0+i*32))
-    vfa.io.frs1  := vfa_vsrc(1)(0) // VS1(63,0)
-    vfa.io.fp_b := src2
-    // TODO: change mask
-    val maskTemp = Cat(src3(48),src3(32),src3(16),src3(0))
-    vfa.io.mask := Mux1H(
-      Seq(
-        (sew === 1.U) -> maskTemp,
-        (sew === 2.U) -> Cat(maskTemp(2),maskTemp(0)),
-        (sew === 3.U) -> maskTemp(0)
-      )
-    )
-    vfa.io.uop_idx := uop_idx(0)
-    // TODO: which module to handle dest's original value
-    vfa.io.round_mode   := rm
-    vfa.io.fp_format    := sew
-    vfa.io.opb_widening := src_widen
-    vfa.io.res_widening := widen
-    vfa.io.is_frs1      := is_frs1
-    vfa.io.op_code      := opcode
-    vfa.io.is_vec       := true.B // TODO: check it
-    vfa.io.fp_aIsFpCanonicalNAN := false.B
-    vfa.io.fp_bIsFpCanonicalNAN := false.B
-    vfa.io.maskForReduction := 0.U
-    vfa.io.is_vfwredosum := false.B
-    vfa.io.is_fold := 0.U
-    vfa.io.vs2_fold := Cat(in.src(0)(1), in.src(0)(0))
-    vfa_result.result(i) := vfa.io.fp_result
-    vfa_result.fflags(i) := vfa.io.fflags
-    vfa_result.vxsat := 0.U // DontCare
-    
-  }
+  vfa.io.fire := 0.U // need to change
+  vfa.io.vs1  := in.vSrc(0)
+  vfa.io.vs2  := in.vSrc(1)
+  vfa.io.frs1 := in.rs1
+  vfa.io.is_frs1 := in.vuop.ctrl.vx
+  vfa.io.mask := in.vSrc(3)
+  vfa.io.uop_idx := in.vuop.uopIdx // TODO: check it
+  vfa.io.round_mode := in.vuop.csr.frm //TODO: check it
+  vfa.io.fp_format := in.vuop.csr.vsew(1, 0)
+  vfa.io.opb_widening := in.vuop.ctrl.widen //
+  vfa.io.res_widening := in.vuop.ctrl.widen2 //
+  vfa.io.op_code := vfa_uop.op
+  vfa.io.is_vec := in.vuop.ctrl.vv
+  vfa.io.fp_aIsFpCanonicalNAN := false.B
+  vfa.io.fp_bIsFpCanonicalNAN := false.B
+  vfa.io.maskForReduction := 0.U
+  vfa.io.is_vfwredosum := false.B
+  vfa.io.is_fold := 0.U
+  vfa.io.vs2_fold := 0.U
 
+  vfa_out.vRes := vfa.io.result
+  vfa_out.vfflags := vfa.io.fflags
+
+  val futype = Cat(in.vuop.ctrl.vfadd, in.vuop.ctrl.vfma, in.vuop.ctrl.vfdiv, in.vuop.ctrl.vfcvt)
+
+  io.out := Mux1H(futype, 
+  Seq(
+    vfa_out,
+    vdiv_out
+  ))
 }
