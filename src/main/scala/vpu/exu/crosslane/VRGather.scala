6666ce5fa9ec4c05be89a941a77fc9c4af514264
jlong299 <jlong299@163.com>
2025-05-07 19:04:17 +0800

    vrgather broadcast support lmul=2

--- src/main/scala/vpu/exu/crosslane/VRGather.scala
+++ src/main/scala/vpu/exu/crosslane/VRGather.scala
@@ -20,7 +20,7 @@ class VRGather extends Module {
   })
 
   /**
-    * So far, only vrgather.vi vd, vs2, 0 (lmul=1) is supported, which performs
+    * So far, only vrgather.vi vd, vs2, 0 is supported, which performs
     *    "broadcast vs2[0] to vd"
     */
   // Check if it is vrgather.vi vd, vs2, 0
@@ -30,7 +30,22 @@ class VRGather extends Module {
   val vrgather_vi = uop.ctrl.funct6 === "b001100".U && uop.ctrl.funct3(0)
   val uimmIs0 = uop.ctrl.vs1_imm === 0.U
   assert((io.in.valid && vrgather_vi && uimmIs0) || !io.in.valid, "For vrgather, only vrgather.vi vd, vs2, 0 is supported")
-  val vd_64b = Mux1H(sewOH.oneHot, Seq(8, 16, 32, 64).map(n => Fill(XLEN/n, vs2(n-1, 0))))
+
+  // ---- Only for vrgather.vi vd, vs2, 0  !!!! ----
+  // vs2_0_0 is the lowest 64-bit value of vs2[0]
+  val vs2_0_0_reg = Reg(UInt(64.W))
+  val keep_vs2_0_0 = RegInit(false.B)
+  when (io.in.valid) {
+    when (uop.uopEnd) {
+      keep_vs2_0_0 := false.B
+    }.otherwise {
+      keep_vs2_0_0 := true.B
+    }
+  }
+  when (!keep_vs2_0_0) { vs2_0_0_reg := vs2(63, 0) }
+  val vs2_0_0 = Mux(keep_vs2_0_0, vs2_0_0_reg, vs2(63, 0))
+
+  val vd_64b = Mux1H(sewOH.oneHot, Seq(8, 16, 32, 64).map(n => Fill(XLEN/n, vs2_0_0(n-1, 0))))
   val vd_broadcast = Fill(VLEN/64, vd_64b)
   io.out.bits.vd := vd_broadcast
 
