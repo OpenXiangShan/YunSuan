0307008ca60c059840701231cb24386c83d76e5c
jlong299 <jlong299@163.com>
2025-05-29 16:09:27 +0800

    Rewrite debugROB to support interleaved writeback

--- src/main/scala/vpu/debug/DebugRob.scala
+++ src/main/scala/vpu/debug/DebugRob.scala
@@ -40,8 +40,8 @@ class DebugRob extends Module {
   val robUop = Reg(Vec(robSize, new VUop))
   val robData = Reg(Vec(robSize, Vec(robEntry_dataDepth, UInt(VLEN.W))))
   val robValid = RegInit(VecInit.fill(robSize)(false.B))
-//   val robUopEnd = RegInit(VecInit.fill(robSize)(false.B))
-  val robWrRf = RegInit(VecInit.fill(robSize)(false.B))
+  val robUopEnd = RegInit(VecInit.fill(robSize)(false.B))
+  // val robWrRf = RegInit(VecInit.fill(robSize)(false.B))
 
   // Find a one-hot enqueue (writeback port 0)
   val enqOH_wbPort_exu = FindLowestOneHot(~(robValid.asUInt))
@@ -56,76 +56,98 @@ class DebugRob extends Module {
   /**
     * Enqueue
     */
+  // Store指令，此处认为在发射时，就已经可以写回rob了。只有在uopEnd时才写回。
   when (io.fromCtrl.wbStore.valid && io.fromCtrl.wbStore.bits.uop.uopEnd) {
     for (i <- 0 until robSize) {
       when (enqOH_wbPort_store(i)) {
         robUop(i) := io.fromCtrl.wbStore.bits.uop
         robValid(i) := true.B
-        // robUopEnd(i) := true.B
-        robWrRf(i) := false.B
+        robUopEnd(i) := true.B
+        // robWrRf(i) := false.B
       }
     }
   }
 
   val wbExuUop = io.fromCtrl.wbExu.bits.uop
-  //---- 在当前指令的第一个有效的uop写回时，记录enqOH_wbPort_exu并保持。直到下一条指令的第一个有效uop写回时，再记录新的enqOH_wbPort_exu
-  val exu_wb_STATE = RegInit(false.B)
-  val enqOH_wbPort_exu_reg = Reg(chiselTypeOf(enqOH_wbPort_exu))
-  when (io.fromCtrl.wbExu.valid && wbExuUop.uopEnd) {
-    exu_wb_STATE := false.B
-  }.elsewhen (io.fromCtrl.wbExu.valid && wbExuUop.ldestValUop) {
-    exu_wb_STATE := true.B
-  }
-  // 当前指令的第一个有效的uop写回
-  val exu_wb_start = io.fromCtrl.wbExu.valid && wbExuUop.ldestValUop && exu_wb_STATE === false.B
-  when (exu_wb_start) {
-    enqOH_wbPort_exu_reg := enqOH_wbPort_exu
-  }
-  val enqOH_wbPort_exu_final = Mux(exu_wb_start, enqOH_wbPort_exu, enqOH_wbPort_exu_reg)
-  // -------------
+  // //---- 在当前指令的第一个有效的uop写回时，记录enqOH_wbPort_exu并保持。直到下一条指令的第一个有效uop写回时，再记录新的enqOH_wbPort_exu
+  // val exu_wb_STATE = RegInit(false.B)
+  // val enqOH_wbPort_exu_reg = Reg(chiselTypeOf(enqOH_wbPort_exu))
+  // when (io.fromCtrl.wbExu.valid && wbExuUop.uopEnd) {
+  //   exu_wb_STATE := false.B
+  // }.elsewhen (io.fromCtrl.wbExu.valid && wbExuUop.ldestValUop) {
+  //   exu_wb_STATE := true.B
+  // }
+  // // 当前指令的第一个有效的uop写回
+  // val exu_wb_start = io.fromCtrl.wbExu.valid && wbExuUop.ldestValUop && exu_wb_STATE === false.B
+  // when (exu_wb_start) {
+  //   enqOH_wbPort_exu_reg := enqOH_wbPort_exu
+  // }
+  // val enqOH_wbPort_exu_final = Mux(exu_wb_start, enqOH_wbPort_exu, enqOH_wbPort_exu_reg)
+  // // -------------
 
+  
+  // Exu uop写回，查询如果已有相同robIdx的uop写回，则更新已有entry。否则更新enqOH_wbPort_exu所指entry。
   when (io.fromCtrl.wbExu.valid) {
+    // 和已有robIdx的所有entry比较
+    val robIdx_match_OH_exu = robUop.zipWithIndex.map { case (uop, idx) => uop.robIdx === wbExuUop.robIdx && robValid(idx) }
+    val robIdx_match_hit_exu = robIdx_match_OH_exu.reduce(_ || _)
     for (i <- 0 until robSize) {
-      when (enqOH_wbPort_exu_final(i) && wbExuUop.ldestValUop) {
+      when(robIdx_match_hit_exu) { // 已有相同robIdx的uop写回
+        when (robIdx_match_OH_exu(i) && wbExuUop.ldestValUop) {
+          robData(i)(wbExuUop.ldestUop - wbExuUop.ctrl.ldest) := io.fromCtrl.wbExu.bits.vd
+        }
+        when (robIdx_match_OH_exu(i) && wbExuUop.uopEnd) {
+          robUopEnd(i) := true.B
+        }
+      }.elsewhen (enqOH_wbPort_exu(i) && wbExuUop.ldestValUop) { // 没有相同robIdx的entry, 这是第一个uop写回
         robData(i)(wbExuUop.ldestUop - wbExuUop.ctrl.ldest) := io.fromCtrl.wbExu.bits.vd
-      }
-      when (enqOH_wbPort_exu_final(i) && wbExuUop.uopEnd) {
         robUop(i) := wbExuUop
         robValid(i) := true.B
-        // robUopEnd(i) := true.B
-        robWrRf(i) := wbExuUop.ctrl.ldestVal
+        when (wbExuUop.uopEnd) {
+          robUopEnd(i) := true.B
+        }
       }
     }
   }
 
 
   val wbLoadUop = io.fromCtrl.wbLoad.bits.uop
-  //---- 在当前指令的第一个有效的uop写回时，记录enqOH_wbPort_load并保持。直到下一条指令的第一个有效uop写回时，再记录新的enqOH_wbPort_load
-  val load_wb_STATE = RegInit(false.B)
-  val enqOH_wbPort_load_reg = Reg(chiselTypeOf(enqOH_wbPort_load))
-  when (io.fromCtrl.wbLoad.valid && wbLoadUop.ldestValUop) {
-    load_wb_STATE := true.B
-  }.elsewhen (io.fromCtrl.wbLoad.valid && wbLoadUop.uopEnd) {
-    load_wb_STATE := false.B
-  }
-  // 当前指令的第一个有效的uop写回
-  val load_wb_start = io.fromCtrl.wbLoad.valid && wbLoadUop.ldestValUop && load_wb_STATE === false.B
-  when (load_wb_start) {
-    enqOH_wbPort_load_reg := enqOH_wbPort_load
-  }
-  val enqOH_wbPort_load_final = Mux(load_wb_start, enqOH_wbPort_load, enqOH_wbPort_load_reg)
-  // -------------
+  // //---- 在当前指令的第一个有效的uop写回时，记录enqOH_wbPort_load并保持。直到下一条指令的第一个有效uop写回时，再记录新的enqOH_wbPort_load
+  // val load_wb_STATE = RegInit(false.B)
+  // val enqOH_wbPort_load_reg = Reg(chiselTypeOf(enqOH_wbPort_load))
+  // when (io.fromCtrl.wbLoad.valid && wbLoadUop.ldestValUop) {
+  //   load_wb_STATE := true.B
+  // }.elsewhen (io.fromCtrl.wbLoad.valid && wbLoadUop.uopEnd) {
+  //   load_wb_STATE := false.B
+  // }
+  // // 当前指令的第一个有效的uop写回
+  // val load_wb_start = io.fromCtrl.wbLoad.valid && wbLoadUop.ldestValUop && load_wb_STATE === false.B
+  // when (load_wb_start) {
+  //   enqOH_wbPort_load_reg := enqOH_wbPort_load
+  // }
+  // val enqOH_wbPort_load_final = Mux(load_wb_start, enqOH_wbPort_load, enqOH_wbPort_load_reg)
+  // // -------------
 
+  // Load指令，uop写回，查询如果已有相同robIdx的uop写回，则更新已有entry。否则更新enqOH_wbPort_load所指entry。
   when (io.fromCtrl.wbLoad.valid) {
+    // 和已有robIdx的所有entry比较
+    val robIdx_match_OH_load = robUop.zipWithIndex.map { case (uop, idx) => uop.robIdx === wbLoadUop.robIdx && robValid(idx) }
+    val robIdx_match_hit_load = robIdx_match_OH_load.reduce(_ || _)
     for (i <- 0 until robSize) {
-      when (enqOH_wbPort_load_final(i) && wbLoadUop.ldestValUop) {
+      when(robIdx_match_hit_load) { // 已有相同robIdx的uop写回
+        when (robIdx_match_OH_load(i) && wbLoadUop.ldestValUop) {
+          robData(i)(wbLoadUop.ldestUop - wbLoadUop.ctrl.ldest) := io.fromCtrl.wbLoad.bits.vd
+        }
+        when (robIdx_match_OH_load(i) && wbLoadUop.uopEnd) {
+          robUopEnd(i) := true.B
+        }
+      }.elsewhen (enqOH_wbPort_load(i) && wbLoadUop.ldestValUop) { // 没有相同robIdx的entry, 这是第一个uop写回
         robData(i)(wbLoadUop.ldestUop - wbLoadUop.ctrl.ldest) := io.fromCtrl.wbLoad.bits.vd
-      }
-      when (enqOH_wbPort_load_final(i) && wbLoadUop.uopEnd) {
         robUop(i) := wbLoadUop
         robValid(i) := true.B
-        // robUopEnd(i) := true.B
-        robWrRf(i) := wbLoadUop.ctrl.ldestVal
+        when (wbLoadUop.uopEnd) {
+          robUopEnd(i) := true.B
+        }
       }
     }
   }
@@ -143,8 +165,8 @@ class DebugRob extends Module {
   val deq_globalRobIdxValid = globalRobIdxFIFO.io.deq.valid
 
   // CAM if global robIdx matched
-  val globalRobIdx_match_OH = robUop zip robValid map { case (uop, valid) =>
-    uop.robIdx === deq_globalRobIdx && valid
+  val globalRobIdx_match_OH = robUop zip robValid zip robUopEnd map { case ((uop, valid), uopEnd) =>
+    uop.robIdx === deq_globalRobIdx && valid && uopEnd
   }
   globalRobIdxFIFO.io.deq.ready := globalRobIdx_match_OH.reduce(_ || _)
   
@@ -160,6 +182,7 @@ class DebugRob extends Module {
     for (i <- 0 until robSize) {
       when (globalRobIdx_match_OH(i)) {
         robValid(i) := false.B
+        robUopEnd(i) := false.B
       }
     }
   }
